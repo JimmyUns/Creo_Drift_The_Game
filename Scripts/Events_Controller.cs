34bc8cfac281e931edef21ef5a2b388de7dc5e28
JimmyUns <jimmyyounes8@gmail.com>
2024-02-21 15:22:38 +0200

    Fix's
    -Moved The functions inside the events script.
    -Added Music Array for checkpoints

--- Scripts/Events_Controller.cs
+++ Scripts/Events_Controller.cs
@@ -5,16 +5,20 @@ using System.Diagnostics;
 public partial class Events_Controller : Node3D
 {
 	public float currentTime;
-	private int eventsIndex;
-	private int lastEventsIndex;
+	private int eventsIndex, lastEventsIndex;
+	[Export] private Node3D playerBody; //For Debugging
 
-	[Export] private Node3D playerPos; //For Debugging
+	//Controllers
 	[Export] private vehiclebody_Controller vhController;
 	[Export] private Camera_Controller camController;
+	//Animation Players
 	[Export] private AnimationPlayer eventAnim;
 	[Export] public AnimationPlayer uiAnim;
-	[Export] private AudioStreamPlayer gameMusic;
+	//Nodes
 	[Export] private Camera3D mainCamera;
+	[Export] private Node3D camHolder;
+	[Export] private AudioStreamPlayer[] gameMusic;
+	[Export] private TextureRect brokenGlassTextureRect;
 
 	public override void _Ready()
 	{
@@ -24,6 +28,25 @@ public partial class Events_Controller : Node3D
 		if (Input.IsActionJustPressed("Death"))
 		{
 			StopGame();
+		}
+
+		if (Input.IsActionJustPressed("Skip"))
+		{
+			CheckPointSpawn();
+
+			mainCamera.Position = Vector3.Zero;
+			mainCamera.RotationDegrees = Vector3.Zero;
+
+			brokenGlassTextureRect.Visible = false;
+		}
+		else if (Input.IsActionJustPressed("Skip2"))
+		{
+			StartingCutsceneAnimation();
+
+			mainCamera.Position = Vector3.Zero;
+			mainCamera.RotationDegrees = Vector3.Zero;
+
+			brokenGlassTextureRect.Visible = false;
 		}
 
 		if (eventsIndex == 0) return;
@@ -74,8 +97,8 @@ public partial class Events_Controller : Node3D
 		switch (lastEventsIndex)
 		{
 			case 2: //Billboard Animation
-				playerPos.GlobalPosition = new Vector3(0f, -5.7153025f, -473f);
-				playerPos.Visible = true;
+				playerBody.GlobalPosition = new Vector3(0f, -5.7153025f, -473f);
+				playerBody.Visible = true;
 				currentTime = 12.1f;
 				eventsIndex = 3;
 
@@ -83,14 +106,13 @@ public partial class Events_Controller : Node3D
 				vhController.inputEnabled = false;
 				camController.isActive = false;
 				eventAnim.Play("Billboard_Event");
+
+				gameMusic[1].Play();
 				break;
 		}
 	}
 
-	public void StartTime()
-	{
-		eventsIndex = 1;
-	}
+
 
 	public void BBEvent2()
 	{
@@ -110,7 +132,28 @@ public partial class Events_Controller : Node3D
 		currentTime = 0f;
 		vhController.isActive = false;
 		vhController.vBody.LinearVelocity = Vector3.Zero;
-		gameMusic.Stop();
+		foreach (AudioStreamPlayer gameM in gameMusic)
+		{
+			gameM.Stop();
+		}
 		eventAnim.Play("Player_Death");
+	}
+
+	public void StartingCutsceneAnimation()
+	{
+		mainCamera.GetNode<Camera3D>(mainCamera.GetPath()).Fov = 55f;
+		eventAnim.Play("Game Starting Cutscene Animation");
+		gameMusic[0].Play();
+		eventsIndex = 1;
+	}
+
+	public void StartGameplay()
+	{
+		camHolder.GlobalRotationDegrees = Vector3.Zero;
+		mainCamera.Fov = 90f;
+
+		camController.isActive = true;
+		vhController.isActive = true;
+		vhController.inputEnabled = true;
 	}
 }
--- Scripts/GUI/MainMenuFunctions.cs
+++ Scripts/GUI/MainMenuFunctions.cs
@@ -4,16 +4,8 @@ using System.Diagnostics;
 
 public partial class MainMenuFunctions : CanvasLayer
 {
-	[Export] private Camera3D mainCamera;
-	[Export] private Node3D camHolder;
 	[Export] private AnimationPlayer mainmenuCamAnim;
 	[Export] private Node3D mainmenu_Map, game_Map;
-	[Export] private AudioStreamPlayer gameMusic;
-	[Export] private TextureRect brokenGlassTextureRect;
-
-
-	[Export] private Camera_Controller camController;
-	[Export] private vehiclebody_Controller vehicleBodyController;
 	[Export] private Events_Controller eventsController;
 
 	// Called when the node enters the scene tree for the first time.
@@ -29,27 +21,7 @@ public partial class MainMenuFunctions : CanvasLayer
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("Skip"))
-		{
-			GetNode<CanvasLayer>(this.GetPath()).Visible = false;
-
-			eventsController.CheckPointSpawn();
-			
-			mainCamera.Position = Vector3.Zero;
-			mainCamera.RotationDegrees = Vector3.Zero;
-			
-			brokenGlassTextureRect.Visible = false;
-		} else if (Input.IsActionJustPressed("Skip2"))
-		{
-			GetNode<CanvasLayer>(this.GetPath()).Visible = false;
-
-			StartingCutsceneAnimation();
-			
-			mainCamera.Position = Vector3.Zero;
-			mainCamera.RotationDegrees = Vector3.Zero;
-			
-			brokenGlassTextureRect.Visible = false;
-		}
+		
 	}
 
 	public void _on_button_button_down()
@@ -63,20 +35,7 @@ public partial class MainMenuFunctions : CanvasLayer
 	{
 		mainmenu_Map.Visible = false;
 		game_Map.Visible = true;
-		mainCamera.GetNode<Camera3D>(mainCamera.GetPath()).Fov = 55f;
-		mainmenuCamAnim.Play("Game Starting Cutscene Animation");
-		gameMusic.Play();
-		eventsController.StartTime();
-	}
-
-	public void StartGameplay()
-	{
-		//mainmenuCamAnim.Stop();
-		camHolder.GlobalRotationDegrees = Vector3.Zero;
-		mainCamera.Fov = 90f;
-
-		camController.isActive = true;
-		vehicleBodyController.isActive = true;
-		vehicleBodyController.inputEnabled = true;
+		
+		eventsController.StartingCutsceneAnimation();
 	}
 }
